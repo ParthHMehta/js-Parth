b0875f08ee0022ae326b409da429651acaa5d1fa
Parth <130344699+ParthHMehta@users.noreply.github.com>
2025-04-02 10:07:24 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+// tc39 and mdn are the two sites to study js (ECMAStandards) 
+"use strict"; // terat all js code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high 
+
+console.log("Parth")
+
+
+let name ="Parth"
+let age = 21
+let isLoggedIn = false
+
+// Datatypes in js
+ /*
+ number => 2 to the power 53 
+ bigint 
+ string => ""
+ boolean => true/false
+ null => standalone value
+ undefined => value not defined *it is a object*
+ symbol => used to find uniqueness
+*/
+
+
+// obejct
+
+console.log(typeof undefined); //undefined 
+console.log(typeof"null"); // null is a object
