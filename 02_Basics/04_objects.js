7f4942a7aa225036ac719c3ff72ebc1c5abc336a
Parth <130344699+ParthHMehta@users.noreply.github.com>
2025-04-14 08:54:32 +0000

    objects part 2

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,61 @@
+// const tinderuser = new Object()  a singleton obejct 
+const tinderuser = {}   //a non-singleton obeject the only internal obeject 
+
+tinderuser.id = "123abc"
+tinderuser.name = "Parth"
+tinderuser. isLoggedIn = false 
+
+// console.log(tinderuser);
+
+const regularuser ={
+    email: "some@gmail.com",
+    fullname: {
+        username: {
+            firstname: "Parth",
+            lastname: "Mehta",
+        }
+    }
+}
+
+// console.log(regularuser.fullname.username);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+const obj4 = {...obj1, ...obj2, ...obj3}
+
+// console.log(obj4);
+
+const users = [
+    {
+        id : 1,
+        email : "P@gmail.com",
+    },
+    {
+        id : 1,
+        email : "P@gmail.com",
+    },
+    {
+        id : 1,
+        email : "P@gmail.com",
+    },
+]
+
+users[1].email
+// console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.keys(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLogged'));
+
+
+
+
+
+
+
+
