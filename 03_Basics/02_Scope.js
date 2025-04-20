ca01a5ab9f3240e09666cf8217fe8e91bbcb51ff
Parth <130344699+ParthHMehta@users.noreply.github.com>
2025-04-20 11:29:15 +0000

    js

--- 03_Basics/02_Scope.js
+++ 03_Basics/02_Scope.js
@@ -1,49 +1,49 @@
-// var a = 100
-// if (true) {
-//     const a = 10
-//     let b = 20
-//     var c = 30
+var a = 100
+if (true) {
+    const a = 10
+    let b = 20
+    var c = 30
 
-//     // console.log("Inner:" ,a);  
-// }
-// console.log(a);
-// console.log(b);
-// console.log(c);
+    console.log("Inner:" ,a);  
+}
+console.log(a);
+console.log(b);
+console.log(c);
 
 
-// function one (){
-//     const username = "Parth"
+function one (){
+    const username = "Parth"
     
-//     function two (){
-//         const website = "Youtube"
-//         console.log(username);
+    function two (){
+        const website = "Youtube"
+        console.log(username);
         
-//     }
-//     // console.log(website);
+    }
+    console.log(website);
 
-//     two()
+    two()
 
-// }
+}
 
-// one()
+one()
 
-// if(true){
-//     const username = "Parth"
-//     if(username==="Parth"){
-//         const website = " Youtube"
-//         console.log(username + website);
-        // console.log(website);
+if(true){
+    const username = "Parth"
+    if(username==="Parth"){
+        const website = " Youtube"
+        console.log(username + website);
+        console.log(website);
         
-        // }
-        // console.log(website); X
-        // console.log(username);
-// }
-// console.log(username); X
+        }
+        console.log(website); X
+        console.log(username);
+}
+console.log(username); X
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++ intresting ++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// addOne(5) can be used before initialization
+addOne(5) //can be used before initialization
 
 function addOne (num){
     return num + 1
@@ -51,7 +51,7 @@ function addOne (num){
 
 
 
-// addTwo(5)  cannot be used before initialization
+addTwo(5)  //cannot be used before initialization
 const addTwo = function(num){
     return num + 2
 }
---
+++ 03_Basics/03_arrowfunction.js
@@ -0,0 +1,44 @@
+const user = {
+    username: "Parth",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , Welcome to website`);
+        console.log(this);
+        
+    }
+
+}
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Parth"
+//     console.log(this.username);  
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "Parth"
+//         console.log(this.username);  
+//     }
+//     chai()
+
+const chai = () => {
+    let username = "Parth"
+        console.log(this);  
+    }
+    // chai()
+    
+// ()=>{}   arrow function
+
+// const addTwo = (num1, num2) => num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)
+const addTwo = (num1, num2) => ({username: "Parth"})
+
+
+console.log(addTwo(3, 4));
+
