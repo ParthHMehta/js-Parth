42fc725db514f27baaf28cf18196f81aa4e28791
ParthHMehta <parthmehta215@gmail.com>
2025-05-13 14:10:24 +0530

    Loops

--- 05_Iterations/Loops.js
+++ 05_Iterations/Loops.js
@@ -1,9 +1,9 @@
-// for 
+// for
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
     if (element == 5) {
-        //console.log("Five"); 
+        //console.log("Five");
     }
     //console.log(element);
     
@@ -13,7 +13,7 @@ for (let i = 1; i <= 10; i++) {
     //console.log(`outer loop value: ${i}`);
     for (let j = 1; j <= 10; j++) {
         //console.log(`inner loop value:${j} and inner loop valus ${i}`);
-        //console.log(`${i} * ${j} = ${i*j}`);    
+        //console.log(`${i} * ${j} = ${i*j}`);
     }
     
 }
